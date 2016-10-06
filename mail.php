9ac684cda1d245741c684e49af24e6401262a5b8
Santiago Moreno <smoreno91@gmail.com>
2016-10-06 15:55:39 +0000

    Adding some changes from liebre

--- mail.php
+++ mail.php
@@ -1,16 +1,25 @@
 <?php
 if(isset($_POST['email'])){
-		$mailTo = "elixir@jellythemes.com";
-		$subject = "mail from web";
-		$body = "New message from web
+	$mailTo = "info@lamatriarca,co";
+	$subject = "Correo desde www.lamatriarca.co";
+	
+	$postEmail	 = $_POST['email'];
+	$postName	 = $_POST['name'];
+	$postSubject = $_POST['subject'];
+	$postMessage = $_POST['message'];
+	
+	$body = "Nuevo mensaje enviado desde la página web
 <br><br>
-FROM: ".$_POST['email']."<br>
-NAME: ".$_POST['name']."<br>
-COMMENTS: ".$_POST['message']."<br>";	
-		$headers = "To: Elixir <".$mailTo.">\r\n";
-		$headers .= "From: ".$_POST['author']." <".$_POST['email'].">\r\n";
-		$headers .= "Content-Type: text/html";
-		//envio destinatario
-		$mail_success =  mail($mailTo, utf8_decode($subject), utf8_decode($body), $headers);		
+CORREO: ".$postEmail."<br>
+NOMBRE: ".$postName."<br>
+ASUNTO: ".$postSubject."<br>
+MENSAJE: ".$postMessage = $_POST['message']."<br>";	
+
+	$headers = "To: Restaurante La Matriarca <".$mailTo.">\r\n";
+	$headers .= "From: ".$postName." <".$postEmail.">\r\n";
+	$headers .= "Content-Type: text/html";
+	
+	//envio destinatario
+	$mail_success =  mail($mailTo, utf8_decode($subject), utf8_decode($body), $headers);		
 }
 ?>  
